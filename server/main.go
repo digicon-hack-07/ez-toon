a7dd72601002a236d6de47d2ddb37d164f5e2239
logica <84759675+logica0419@users.noreply.github.com>
2022-09-24 19:28:15 +0900

    setup server

---
+++ server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.Debug = true
+
+	e.GET("/ping", func(c echo.Context) error {
+		return c.String(http.StatusOK, "pong")
+	})
+
+	e.Logger.Panic(e.Start(":8080"))
+}
