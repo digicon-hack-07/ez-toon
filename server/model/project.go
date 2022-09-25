578d4b90c62dfa92f8e523b9cbfdc9790a9c650e
logica <84759675+logica0419@users.noreply.github.com>
2022-09-25 18:13:08 +0000

    fix project

--- server/model/project.go
+++ server/model/project.go
@@ -1,10 +1,16 @@
 package model
 
-import "github.com/oklog/ulid/v2"
+import (
+	"time"
+
+	"github.com/oklog/ulid/v2"
+)
 
 type Project struct {
 	ID        ulid.ULID
 	Name      string
 	Pages     int
 	Thumbnail string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
