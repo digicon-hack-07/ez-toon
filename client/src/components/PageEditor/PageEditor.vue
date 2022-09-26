db2ee3661697ca5b9e8b35dc0beae3e5fda01496
kegra <nukegarara@yahoo.co.jp>
2022-09-26 14:59:40 +0900

    refactor

--- client/src/components/PageEditor/PageEditor.vue
+++ client/src/components/PageEditor/PageEditor.vue
@@ -74,75 +74,75 @@ const pointerdown = (e: PointerEvent) => {
   const by = canvas.value?.getClientRects().item(0)?.top
   if (bx === undefined || by === undefined) return
   switch (mode.value) {
-    case 'move':
-      if (draggingData) break
-      draggingData = {
-        pointerId: e.pointerId,
-        beginX: e.x,
-        beginY: e.y,
-        tgtBeginX: canvasScrollX.value,
-        tgtBeginY: canvasScrollY.value
+  case 'move':
+    if (draggingData) break
+    draggingData = {
+      pointerId: e.pointerId,
+      beginX: e.x,
+      beginY: e.y,
+      tgtBeginX: canvasScrollX.value,
+      tgtBeginY: canvasScrollY.value
+    }
+    break
+  case 'pen':
+    working_path.set(e.pointerId, [
+      {
+        x: e.clientX - bx,
+        y: e.clientY - by
       }
-      break
-    case 'pen':
-      working_path.set(e.pointerId, [
-        {
-          x: e.clientX - bx,
-          y: e.clientY - by
-        }
-      ])
-      break
-    case 'dialogue':
-      dialogues.value.push({
-        id: '',
-        pageID: '',
-        dialogue: '',
-        left: e.clientX - bx - dialogue_default_width,
-        top: e.clientY - by,
-        right: e.clientX - bx,
-        bottom: e.clientY - by + dialogue_default_height
-      })
-      break
+    ])
+    break
+  case 'dialogue':
+    dialogues.value.push({
+      id: '',
+      pageID: '',
+      dialogue: '',
+      left: e.clientX - bx - dialogue_default_width,
+      top: e.clientY - by,
+      right: e.clientX - bx,
+      bottom: e.clientY - by + dialogue_default_height
+    })
+    break
   }
 }
 const pointermove = (e: PointerEvent) => {
   switch (mode.value) {
-    case 'move':
-      if (!draggingData || draggingData.pointerId != e.pointerId) break
-      canvasScrollX.value = e.x - draggingData.beginX + draggingData.tgtBeginX
-      canvasScrollY.value = e.y - draggingData.beginY + draggingData.tgtBeginY
-      break
-    case 'pen':
-      const path = working_path.get(e.pointerId)
-      if (!path) return
+  case 'move':
+    if (!draggingData || draggingData.pointerId != e.pointerId) break
+    canvasScrollX.value = e.x - draggingData.beginX + draggingData.tgtBeginX
+    canvasScrollY.value = e.y - draggingData.beginY + draggingData.tgtBeginY
+    break
+  case 'pen':
+    const path = working_path.get(e.pointerId)
+    if (!path) return
 
-      const bx = canvas.value?.getClientRects().item(0)?.left
-      const by = canvas.value?.getClientRects().item(0)?.top
-      if (bx === undefined || by === undefined) return
+    const bx = canvas.value?.getClientRects().item(0)?.left
+    const by = canvas.value?.getClientRects().item(0)?.top
+    if (bx === undefined || by === undefined) return
 
-      path.push({
-        x: e.clientX - bx,
-        y: e.clientY - by
-      })
-      if (ctx.value) drawPath(ctx.value, path)
-      break
-    case 'dialogue':
-      break
+    path.push({
+      x: e.clientX - bx,
+      y: e.clientY - by
+    })
+    if (ctx.value) drawPath(ctx.value, path)
+    break
+  case 'dialogue':
+    break
   }
 }
 const pointerup = (e: PointerEvent) => {
   switch (mode.value) {
-    case 'move':
-      draggingData = null
-      break
-    case 'pen':
-      const path = working_path.get(e.pointerId)
-      if (!path) return
+  case 'move':
+    draggingData = null
+    break
+  case 'pen':
+    const path = working_path.get(e.pointerId)
+    if (!path) return
 
-      paths.push(path)
-      working_path.delete(e.pointerId)
-    case 'dialogue':
-      break
+    paths.push(path)
+    working_path.delete(e.pointerId)
+  case 'dialogue':
+    break
   }
 }
 
