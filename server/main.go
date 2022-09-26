e7ab08b6f8c4f12664d3851eec42f6aea5f9e790
logica <84759675+logica0419@users.noreply.github.com>
2022-09-27 04:07:05 +0900

    divide router

--- server/main.go
+++ server/main.go
@@ -1,19 +1,8 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/labstack/echo/v4"
-)
+import "github.com/digicon-hack-07/ez-toon/server/router"
 
 func main() {
-	e := echo.New()
-	e.Debug = true
-
-	e.GET("/ping", func(c echo.Context) error {
-		return c.String(http.StatusOK, "pong")
-	})
-	e.Static("/", "public")
-
-	e.Logger.Panic(e.Start(":8080"))
+	r := router.NewRouter()
+	r.Start()
 }
---
+++ server/router/router.go
@@ -0,0 +1,38 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
+)
+
+type Router struct {
+	e *echo.Echo
+}
+
+func NewRouter() *Router {
+	e := echo.New()
+	e.Debug = true
+	e.Logger.SetLevel(log.DEBUG)
+	e.Logger.SetHeader("${time_rfc3339} ${prefix} ${short_file} ${line} |")
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "${time_rfc3339} method = ${method} | uri = ${uri} | status = ${status} ${error}\n",
+	}))
+
+	e.Use(session.Middleware(sessions.NewCookieStore([]byte("secret"))))
+
+	e.GET("/ping", func(c echo.Context) error {
+		return c.String(http.StatusOK, "pong")
+	})
+	e.Static("/", "public")
+
+	return &Router{e: e}
+}
+
+func (r *Router) Start() {
+	r.e.Logger.Panic(r.e.Start(":8080"))
+}
