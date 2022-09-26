e11eb023e7beddd367cb248b76b15ab7256785ea
kegra <nukegarara@yahoo.co.jp>
2022-09-26 20:08:13 +0900

    added drawLine()

--- client/src/lib/renderer/path.ts
+++ client/src/lib/renderer/path.ts
@@ -1,3 +1,5 @@
+import { Line } from "../line"
+
 export type PathRenderData = {
   x: number
   y: number
@@ -7,6 +9,16 @@ export function drawPath(ctx: CanvasRenderingContext2D, path: PathRenderData) {
   ctx.beginPath()
   ctx.moveTo(path[0].x, path[0].y)
   for (const pos of path) {
+    ctx.lineTo(pos.x, pos.y)
+  }
+  ctx.stroke()
+  ctx.closePath()
+}
+
+export function drawLine(ctx: CanvasRenderingContext2D, line: Line) {
+  ctx.beginPath()
+  ctx.moveTo(line.path[0].x, line.path[0].y)
+  for (const pos of line.path) {
     ctx.lineTo(pos.x, pos.y)
   }
   ctx.stroke()
