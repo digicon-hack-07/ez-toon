f80fcf2925582b4579ddf5be9b543e66028f05e1
logica <84759675+logica0419@users.noreply.github.com>
2022-09-27 23:31:05 +0900

    fix page handler

--- server/router/page/page.go
+++ server/router/page/page.go
@@ -1,10 +1,28 @@
 package page
 
-import "github.com/oklog/ulid/v2"
+import (
+	"github.com/digicon-hack-07/ez-toon/server/router/content"
+	"github.com/oklog/ulid/v2"
+)
 
 type Page struct {
-	ID     ulid.ULID
-	Index  int
-	Height int
-	Width  int
+	ID     ulid.ULID `json:"id,omitempty"`
+	Index  int       `json:"index,omitempty"`
+	Height int       `json:"height,omitempty"`
+	Width  int       `json:"width,omitempty"`
+}
+
+type PageWithContents struct {
+	ID        ulid.ULID          `json:"id,omitempty"`
+	Index     int                `json:"index,omitempty"`
+	Height    int                `json:"height,omitempty"`
+	Width     int                `json:"width,omitempty"`
+	Lines     []content.Line     `json:"lines,omitempty"`
+	Dialogues []content.Dialogue `json:"dialogues,omitempty"`
+}
+
+type PageHandler struct{}
+
+func NewPageHandler() *PageHandler {
+	return &PageHandler{}
 }
