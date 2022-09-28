409fd20edf0da9a9b366e607f1ad0dcf52245df6
logica <84759675+logica0419@users.noreply.github.com>
2022-09-28 17:01:24 +0900

    implement line repository

--- server/repository/gorm2/line.go
+++ server/repository/gorm2/line.go
@@ -23,3 +23,42 @@ func (repo *Repository) SelectLines(ctx context.Context, pageID ulid.ULID) ([]*r
 	tx.Commit()
 	return lines, nil
 }
+
+func (repo *Repository) InsertLine(ctx context.Context, id ulid.ULID, pageID ulid.ULID, penSize int, points []repository.Point) (*repository.Line, error) {
+	tx, err := repo.getTX(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+
+	line := repository.Line{
+		ID:      id,
+		PageID:  pageID,
+		PenSize: penSize,
+		Points:  points,
+	}
+
+	err = tx.Create(&line).Error
+	if err != nil {
+		return nil, err
+	}
+
+	tx.Commit()
+	return &line, nil
+}
+
+func (repo *Repository) DeleteLine(ctx context.Context, id ulid.ULID) error {
+	tx, err := repo.getTX(ctx)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	err = tx.Where("id = ?", id).Delete(&repository.Line{}).Error
+	if err != nil {
+		return err
+	}
+
+	tx.Commit()
+	return nil
+}
